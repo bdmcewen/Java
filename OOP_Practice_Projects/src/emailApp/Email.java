a25f29773810825914225613400cabd631b0c56f
Ben McEwen <bdmcewen.bm@gmail.com>
2020-10-21 08:40:02 -0600

    email constructor partial set password complete

--- OOP_Practice_Projects/src/emailApp/Email.java
+++ OOP_Practice_Projects/src/emailApp/Email.java
@@ -12,17 +12,26 @@ public class Email {
     private String alternateEmail;
 
 
-
     // Constructor to receive the first and last name
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        System.out.println("Created user: " + this.firstName + " " + this.lastName);
+        System.out.println();
 
         // Call a method asking for the dept + return the dept
         this.department = setDepartment();
+        System.out.println("Dept set: "  + this.department);
+        System.out.println();
+
+        // Call a method that returns a random password
+        this.password = setPassword();
+        System.out.println("Password set: " );
+        System.out.println();
 
         System.out.print(this);
     }
+
 
     // Ask for Department
     private String setDepartment() {
@@ -40,9 +49,25 @@ public class Email {
         } else {
             return "Invalid selection, returning";
         }
+
+        //TODO: implement wait for key press before returning
     }
 
     // generate random password
+    private String setPassword() {
+        Scanner ps1 = new Scanner(System.in);
+        System.out.print("Enter new user password: ");
+        String newPassword1 = ps1.nextLine();
+        Scanner ps2 = new Scanner(System.in);
+        System.out.print("Enter new user password again: ");
+        String newPassword2 = ps2.nextLine();
+        if (newPassword1.equals(newPassword2)) {
+            return newPassword1;
+        } else {
+            return "Error.";
+        }
+        //TODO: Learn how to hash password w/salt & password masking
+    }
 
     // set mailbox capacity
 
